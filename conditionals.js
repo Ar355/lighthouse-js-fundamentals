200f8737ce0bd87de0ff274142e33b0b6b91244d
Ar355 <zeneli.arian@gmail.com>
2020-07-09 23:40:23 +0000

    examples of conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,4 @@
-const temperature = 12;
+const isCitizen = true;
+const age = 26;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
+(isCitizen && age > 18) ? console.log('You are eligeble to vote'); :
