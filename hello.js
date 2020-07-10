fa3d067cbde7a03512bfb0775fec8f2e1aac44db
Ar355 <zeneli.arian@gmail.com>
2020-07-10 13:01:27 +0000

    hello world in js

--- hello.js
+++ hello.js
@@ -2,3 +2,9 @@ const sayHello = function (name) {
   console.log('Hello ' + name);
 }
 sayHello('caliba');
+
+const returnSayHello = function (nam) {
+  return 'hello' + nam;
+}
+const greeting = returnSayHello('Jon');
+console.log(greeting);
